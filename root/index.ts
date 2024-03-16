c79c61b54a67a78e310390590fc43b2f034dc857
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-16 14:43:59 +0700

    my love

---
+++ root/index.ts
@@ -0,0 +1,83 @@
+interface Address {
+  country: string;
+  province: string;
+  city: string;
+  district: string;
+  village: string;
+}
+
+interface Hobbies {
+  [index: number]: string
+}
+
+interface EducationDetail {
+  name: string;
+  level: string;
+  address: string;
+  year: string;
+}
+
+interface Education {
+  [index: number]: EducationDetail
+}
+
+interface FavouriteFood {
+  [index: number]: string;
+}
+
+interface MyLove {
+  name: string;
+  address: Address;
+  age: number;
+  hobbies: Hobbies;
+  educations: Education;
+  favouriteFoods: FavouriteFood;
+}
+
+interface SendLove {
+  (love: MyLove): void;
+}
+
+const myLove: MyLove = {
+  name: "Via Fitriana",
+  address: {
+    country: "Indonesian",
+    province: "Central Java",
+    city: "Cilacap Regency",
+    district: "Sidareja",
+    village: "Cibenon"
+  },
+  age: 19,
+  hobbies: [
+    "drawing",
+    "make up",
+    "play game"
+  ],
+  educations: [
+    {
+      name: "SMK Yos Sidareja",
+      level: "Vacational High School",
+      address: "Cibenon, Sidareja",
+      year: "2020 - 2023"
+    },
+    {
+      name: "SMP Yos Sidareja",
+      level: "Elementary School",
+      address: "Cibenon, Sidareja",
+      year: "2017 - 2020"
+    }
+  ],
+  favouriteFoods: [
+    "ayam geprek",
+    "bakso",
+    "mie ayam",
+    "seblak",
+    "eskrim"
+  ]
+};
+
+const sendLove: SendLove = (love: MyLove): void => {
+  console.info(love);
+}
+
+sendLove(myLove);
